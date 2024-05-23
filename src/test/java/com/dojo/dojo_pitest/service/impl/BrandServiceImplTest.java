a11f91c333a65d65188cc8f898829295f908b578
scvera <scvera@personalsoft.com>
2024-05-23 10:17:03 -0500

    Proyecto Listo para dojo

--- src/test/java/com/dojo/dojo_pitest/service/impl/BrandServiceImplTest.java
+++ src/test/java/com/dojo/dojo_pitest/service/impl/BrandServiceImplTest.java
@@ -55,129 +55,51 @@ class BrandServiceImplTest {
 
     @Test
     void getTest() {
-        Mockito.when(brandRepository.findById(anyLong()))
-                .thenReturn(Optional.of(brandEntity));
-        Mockito.when(iMapper.brandEntityToBrand(any(BrandEntity.class)))
-                .thenReturn(brand);
 
-        Brand brandRta = brandService.get(1L);
-        assertNotNull(brandRta);
-        assertEquals(brandRta, brand);
     }
 
     @Test
     void getEmptyTest() {
-        Mockito.when(brandRepository.findById(anyLong()))
-                .thenReturn(Optional.empty());
-        Mockito.when(iMapper.brandEntityToBrand(any(BrandEntity.class)))
-                .thenReturn(brand);
 
-        assertThrows(ApplicationException.class, () -> brandService.get(1L));
     }
 
     @Test
     void getAllTest() {
-        Mockito.when(brandRepository.findAll())
-                .thenReturn(List.of(brandEntity));
-        Mockito.when(iMapper.brandEntityToBrand(any(BrandEntity.class)))
-                .thenReturn(brand);
 
-        List<Brand> brands = brandService.getAll();
-        assertFalse(brands.isEmpty());
-        assertEquals(1, brands.size());
     }
 
     @Test
     void deleteTest() {
-        Mockito.when(brandRepository.existsById(anyLong()))
-                .thenReturn(true);
-        Mockito.doNothing().when(brandRepository).deleteById(anyLong());
 
-        assertTrue(brandService.delete(1L));
-        verify(brandRepository, times(1)).deleteById(anyLong());
     }
 
     @Test
     void deleteNotExistTest() {
-        Mockito.when(brandRepository.existsById(anyLong()))
-                .thenReturn(false);
 
-        assertThrows(ApplicationException.class, () -> brandService.delete(1L));
     }
 
     @Test
     void createTest() {
-        Mockito.when(brandRepository.findByName(anyString()))
-                .thenReturn(null);
-        Mockito.when(iMapper.brandEntityToBrand(any(BrandEntity.class)))
-                .thenReturn(brand);
-        Mockito.when(brandRepository.save(any(BrandEntity.class)))
-                .thenReturn(brandEntity);
 
-        String brandName = "kia";
-        Brand brandCreated = brandService.create(brandName);
-
-        assertNotNull(brandCreated);
-        assertEquals(brandName.toUpperCase(), brandCreated.getName());
-        verify(iMapper, times(1)).brandEntityToBrand(any(BrandEntity.class));
     }
 
     @Test
     void createAlreadyBrandTest() {
-        Mockito.when(brandRepository.findByName(anyString()))
-                .thenReturn(brandEntity);
 
-        assertThrows(ApplicationException.class, () -> brandService.create(brandName));
     }
 
     @Test
     void updateTest() {
-        var brandDb = new BrandEntity(1L, "KIA");
-        var newBrandEntity = new BrandEntity(1L, "SUBARU");
-        var newBrand = new Brand(1L, "SUBARU");
 
-        Mockito.when(brandRepository.findById(anyLong()))
-                .thenReturn(Optional.of(brandDb));
-        Mockito.when(brandRepository.save(any(BrandEntity.class)))
-                .thenReturn(newBrandEntity);
-        Mockito.when(iMapper.brandToBrandEntity(any(Brand.class)))
-                .thenReturn(newBrandEntity);
-        Mockito.when(iMapper.brandEntityToBrand(any(BrandEntity.class)))
-                .thenReturn(newBrand);
-
-        var brandUpdated = brandService.update(1L, "SUBARU");
-
-        assertEquals(newBrand.getId(), brandUpdated.getId());
-        assertNotEquals(brandDb.getName(), brandUpdated.getName());
-        verify(brandRepository, times(1)).save(any(BrandEntity.class));
     }
 
     @Test
     void updateEmptyValueTest() {
-        var brandDb = new BrandEntity(1L, "KIA");
-        var newBrandEntity = new BrandEntity(1L, "");
-        var newBrand = new Brand(1L, "");
 
-        Mockito.when(brandRepository.findById(anyLong()))
-                .thenReturn(Optional.of(brandDb));
-        Mockito.when(brandRepository.save(any(BrandEntity.class)))
-                .thenReturn(newBrandEntity);
-        Mockito.when(iMapper.brandToBrandEntity(any(Brand.class)))
-                .thenReturn(newBrandEntity);
-        Mockito.when(iMapper.brandEntityToBrand(any(BrandEntity.class)))
-                .thenReturn(newBrand);
-
-        var brandUpdated = brandService.update(1L, "");
-
-        assertEquals(newBrand.getId(), brandUpdated.getId());
-        assertEquals("", brandUpdated.getName());
     }
 
     @Test
     void updateValueExceptionTest() {
-        Mockito.when(brandRepository.findById(anyLong()))
-                .thenReturn(Optional.empty());
 
-        assertThrows(ApplicationException.class, () -> brandService.update(1L, "KIA"));
     }
 }
--- src/test/java/com/dojo/dojo_pitest/service/impl/ModelServiceImplTest.java
+++ src/test/java/com/dojo/dojo_pitest/service/impl/ModelServiceImplTest.java
@@ -57,104 +57,41 @@ class ModelServiceImplTest {
 
     @Test
     void getTest() {
-        Mockito.when(modelRepository.findById(anyLong()))
-                .thenReturn(Optional.of(modelEntity));
-        Mockito.when(iMapper.modelEntityToModel(any(ModelEntity.class)))
-                .thenReturn(model);
 
-        Model modelRta = modelService.get(1L);
-        assertNotNull(modelRta);
-        assertEquals(modelRta, model);
     }
 
     @Test
     void getEmptyTest() {
-        Mockito.when(modelRepository.findById(anyLong()))
-                .thenReturn(Optional.empty());
-        Mockito.when(iMapper.modelEntityToModel(any(ModelEntity.class)))
-                .thenReturn(model);
 
-        assertThrows(ApplicationException.class, () -> modelService.get(1L));
     }
 
     @Test
     void getAllTest() {
-        Mockito.when(modelRepository.findAll())
-                .thenReturn(List.of(modelEntity));
-        Mockito.when(iMapper.modelEntityToModel(any(ModelEntity.class)))
-                .thenReturn(model);
 
-        List<Model> models = modelService.getAll();
-        assertFalse(models.isEmpty());
-        assertEquals(1, models.size());
     }
 
     @Test
     void deleteTest() {
-        Mockito.when(modelRepository.existsById(anyLong()))
-                .thenReturn(true);
-        Mockito.doNothing().when(modelRepository).deleteById(anyLong());
 
-        assertTrue(modelService.delete(1L));
-        verify(modelRepository, times(1)).deleteById(anyLong());
     }
 
     @Test
     void deleteNotExistTest() {
-        Mockito.when(modelRepository.existsById(anyLong()))
-                .thenReturn(false);
 
-        assertThrows(ApplicationException.class, () -> modelService.delete(1L));
     }
 
     @Test
     void createTest() {
-        Mockito.when(modelRepository.findByBrandIdAndName(anyLong(), anyString()))
-                .thenReturn(null);
-        Mockito.when(iMapper.modelEntityToModel(any(ModelEntity.class)))
-                .thenReturn(model);
-        Mockito.when(modelRepository.save(any(ModelEntity.class)))
-                .thenReturn(modelEntity);
 
-        Model modelCreated = modelService.create(modelDTO);
-
-        assertNotNull(modelCreated);
-        assertEquals(modelDTO.getName().toUpperCase(), modelCreated.getName());
     }
 
     @Test
     void createAlreadyModelTest() {
-        Mockito.when(modelRepository.findByBrandIdAndName(anyLong(), anyString()))
-                .thenReturn(modelEntity);
 
-        assertThrows(ApplicationException.class, () -> modelService.create(modelDTO));
     }
 
     @Test
     void updateTest() {
-        var modelDb = new ModelEntity(1L, 1L, "ONIX", 2009);
-        var newModelDb = new ModelEntity(1L, 2L, "AVEO", 2011);
 
-        Mockito.when(modelRepository.findById(anyLong()))
-                .thenReturn(Optional.of(modelDb));
-        Mockito.when(modelRepository.save(any(ModelEntity.class)))
-                .thenReturn(newModelDb);
-        Mockito.when(iMapper.modelEntityToModel(any(ModelEntity.class)))
-                .thenReturn(model);
-        Mockito.when(iMapper.modelToModelEntity(any(Model.class)))
-                .thenReturn(modelEntity);
-
-        var dto = new UpdateModelDTO(1L, 2L, "AVEO", 2011);
-        var modelUpdated = modelService.update(dto);
-
-        assertAll(
-                () -> assertEquals(modelDb.getId(), modelUpdated.getId()),
-                () -> assertEquals(dto.getBrandId(), modelUpdated.getBrandId()),
-                () -> assertEquals(dto.getName(), modelUpdated.getName()),
-                () -> assertEquals(dto.getYear(), modelUpdated.getYear())
-        );
-
-        verify(modelRepository, times(1)).findById(anyLong());
-        verify(modelRepository, times(1)).save(any(ModelEntity.class));
     }
 }
